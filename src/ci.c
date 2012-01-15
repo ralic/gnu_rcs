ad0d523e5334df435e2984928d53111f2581e1de
Thien-Thi Nguyen <ttn@gnuvola.org>
2012-01-15 09:04:46 +0100

    ci: Fix bug: Don't discard working file mtime with ‘-T’.
    Regression introduced 2010-03-13, "Topo-sort top-level;
    eliminate redundant forward func decls".
    
    * src/ci.c (ci_main): Move incorrectly-placed parens in
    sub-conditional-expression from "partial condition" to
    condition; add additional pair of parens around sub-cond-expr.
    
    * tests/Makefile.am (XFAIL_TESTS): Remove t810.

--- src/ci.c
+++ src/ci.c
@@ -1117,9 +1117,9 @@ main (int argc, char **argv)
 
         if (PROB (donerewrite (changedRCS, !Ttimeflag
                                ? (time_t) - 1
-                               : from && wtime < (repo_stat->st_mtime
-                                                  ? repo_stat->st_mtime
-                                                  : wtime))))
+                               : ((from && wtime < repo_stat->st_mtime)
+                                  ? repo_stat->st_mtime
+                                  : wtime))))
           continue;
 
         if (!keepworkingfile)
